ec0e37995075758093b752868fdd98981dd2431b
Guilherme A. de Souza <souzaaugustoguilherme@gmail.com>
2022-08-25 20:15:28 -0300

    Saves labeled images to train the model - capture_images.py

---
+++ capture_images.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import cv2
+import os
+import sys
+
+DESC = '''
+Usage: python capture_images.py <label_name> <num_samples>
+
+Press 'a' to start/pause the image collecting process.
+Press 'q' to quit.
+'''
+
+if __name__ == "__main__":
+    try:
+        label_name = sys.argv[1]
+        num_samples = int(sys.argv[2])
+    except:
+        print("Arguments missing.")
+        print(DESC)
+        exit(-1)
+
+    IMG_SAVE_PATH = 'image_data'
+    IMG_CLASS_PATH = os.path.join(IMG_SAVE_PATH, label_name)
+
+    try:
+        os.mkdir(IMG_SAVE_PATH)
+        os.mkdir(IMG_CLASS_PATH)
+    except FileExistsError:
+        print(f"{IMG_CLASS_PATH} directory already exists, all images will be saved along with existing items in this folder.")
+
+    cap = cv2.VideoCapture(0)
+
+    start = False
+    count = 0
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            continue
+        if count == num_samples:
+            break
+
+        cv2.rectangle(frame, (100, 100), (500, 500), (255, 0, 0), 2)
+
+        if start:
+            roi = frame[100:500, 100:500]
+            save_path = os.path.join(IMG_CLASS_PATH, f"{count + 1}.jpg")
+            cv2.imwrite(save_path, roi)
+            count += 1
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame, "Collecting {}".format(count),
+            (5, 50), font, 0.7, (255, 0, 0), 2, cv2.LINE_AA)
+        cv2.imshow("Collecting images", frame)
+
+        k = cv2.waitKey(10)
+        if k == ord('a'):
+            start = not start
+        if k == ord('q'):
+            break
+
+    print("\n{} image(s) saved to {}".format(count, IMG_CLASS_PATH))
+    cap.release()
+    cv2.destroyAllWindows()
